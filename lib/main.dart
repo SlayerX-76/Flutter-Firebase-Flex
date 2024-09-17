ba831bf839c61bca4da3cbd94bff432655d25133
Alex Magnum <ekwemarchal023@gmail.com>
2024-09-17 02:38:10 +0100

    Yes

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flex_list/home_screen.dart';
+import 'package:flex_list/login_screen.dart';
+import 'package:flex_list/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ToDo",
+      theme: ThemeData(primarySwatch: Colors.indigo, primaryColor: Colors.indigo),
+      home: _auth.currentUser != null? HomeScreen() : LoginScreen(),
+    );
+
+  }
+
+}
+
+
+
